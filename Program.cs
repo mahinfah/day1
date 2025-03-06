e6a7e3180e53455310c54721c2fe6a3294aa2299
mahinfah <mahinfah1234@gmail.com>
2025-03-07 02:02:39 +0600

    huh

--- Program.cs
+++ Program.cs
@@ -1,64 +1,52 @@
 ﻿namespace day1
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Please give input:");
 
-            int currentAssignments = 5;
+            Double s1 = Convert.ToDouble(Console.ReadLine());
 
-            int sophia1 = 93;
-            int sophia2 = 87;
-            int sophia3 = 98;
-            int sophia4 = 95;
-            int sophia5 = 100;
+            Console.WriteLine("First value is "+s1);
 
-            int nicolas1 = 80;
-            int nicolas2 = 83;
-            int nicolas3 = 82;
-            int nicolas4 = 88;
-            int nicolas5 = 85;
+            Double s2 = Convert.ToDouble(Console.ReadLine());
 
-            int zahirah1 = 84;
-            int zahirah2 = 96;
-            int zahirah3 = 73;
-            int zahirah4 = 85;
-            int zahirah5 = 79;
+            Console.WriteLine("Second value is "+s2);
 
-            int jeong1 = 90;
-            int jeong2 = 92;
-            int jeong3 = 98;
-            int jeong4 = 100;
-            int jeong5 = 97;
+            Console.WriteLine("PRESS 1 FOR SUM");
+            Console.WriteLine("PRESS 2 FOR SUBTRACT");
+            Console.WriteLine("PRESS 3 FOR MULTIPICATION");
+            Console.WriteLine("PRESS 4 FOR DIVISION");
+
+            string m = Console.ReadLine();
 
 
-            int sophiaSum = 0;
-            int nicolasSum = 0;
-            int zahirahSum = 0;
-            int jeongSum = 0;
+            if(m== "1")
+            {
+                Console.WriteLine("Sum :" + (s1 + s2));
+            }
+           
+            else if (m == "2")
+            {
+                Console.WriteLine("subtract :" + (s1 - s2));
+            }
+           
+            else if (m == "3")
+         
+            {
+                Console.WriteLine("Multipication :" + (s1 * s2));
+            }
+           
+            else if (m == "4")
+            {
+                Console.WriteLine("Dvision :" + (s1 / s2));
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid operation");
+            }
 
-            sophiaSum = sophia5 + sophia4 + sophia3 + sophia2 + sophia1;
-            zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-            nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-            jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
-                 
-            Console.WriteLine("Sophia: " + sophiaSum);
-            Console.WriteLine("Nicolas: " + nicolasSum);
-            Console.WriteLine("Zahirah: " + zahirahSum);
-            Console.WriteLine("Jeong: " + jeongSum);
-
-
-
-            decimal sophiaScore = (decimal)sophiaSum / currentAssignments;
-            decimal nicolasScore = (decimal)nicolasSum / currentAssignments;
-            decimal zahirahScore = (decimal)zahirahSum / currentAssignments;
-            decimal jeongScore = (decimal)jeongSum / currentAssignments;
-
-            Console.WriteLine("Sophia: " + sophiaScore + " A");
-            Console.WriteLine("Nicolas: " + nicolasScore + " B");
-            Console.WriteLine("Zahirah: " + zahirahScore + " B");
-            Console.WriteLine("Jeong: " + jeongScore + " A");
 
 
 
